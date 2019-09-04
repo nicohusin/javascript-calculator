6f098e4c26072957d3b5930cbe9ad121acf42627
nicohusin <nicohusin@gmail.com>
2019-09-04 11:36:23 +0700

    First initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const digitA = 100
+const digitB = 200.5
+
+const resultAddition = digitA + digitB
+const resultSubstraction = digitA - digitB
+const resultMultiplication = digitA * digitB
+const resultDivison = digitA / digitB
+
+console.log(digitA)
+console.log(digitB)
+
+console.log(resultAddition)
+console.log(resultSubstraction)
+console.log(resultMultiplication)
+console.log(resultDivison)
